b7c4ed321c00b3257c9c9967572221e32a5c9271
Milind <bhattmilindv@gmail.com>
2025-05-11 00:21:38 +0200

    functions

---
+++ TypeScript JS/functions.js
@@ -0,0 +1,9 @@
+function sayMyName(name1)
+{
+  console.log(name1)
+}
+sayMyName("Milind")
+
+//Arrow functions, shorter version of above
+let sayMyName2 = (name2) => console.log(name2)
+sayMyName2("David")
